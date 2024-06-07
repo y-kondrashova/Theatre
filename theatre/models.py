4cb3ec9d4126bc7d4f5755c322fc09aeb72aba5c
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-06-07 17:01:48 +0300

    Create TheatreHall in models.py theatre app

--- theatre/models.py
+++ theatre/models.py
@@ -25,3 +25,12 @@ class Play(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class TheatreHall(models.Model):
+    name = models.CharField(max_length=100)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    def __str__(self):
+        return self.name
