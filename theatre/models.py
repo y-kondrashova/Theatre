45e083ef6e6175a35d3f4d1eb2b4647522091b92
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-06-07 17:18:42 +0300

    Create Performance in models.py theatre app

--- theatre/models.py
+++ theatre/models.py
@@ -34,3 +34,16 @@ class TheatreHall(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Performance(models.Model):
+    play = models.ForeignKey(
+        Play, on_delete=models.CASCADE, related_name="performances"
+    )
+    theatre_hall = models.ForeignKey(
+        TheatreHall, on_delete=models.CASCADE, related_name="performances"
+    )
+    show_time = models.DateTimeField()
+
+    def __str__(self):
+        return
