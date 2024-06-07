c9654c149ddebc526f5252b26a3ec8f576138788
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-06-07 16:50:45 +0300

    Create Genre in models.py theatre app

--- theatre/models.py
+++ theatre/models.py
@@ -7,3 +7,10 @@ class Actor(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
