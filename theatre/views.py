a465134fb58c06e64c5b64bb7b306c90a01d7bc4
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-06-08 12:20:04 +0300

    Add GenreViewSet to views.py theatre app

--- theatre/views.py
+++ theatre/views.py
@@ -2,12 +2,19 @@ from rest_framework import viewsets
 
 from theatre.models import (
     Actor,
+    Genre,
 )
 from theatre.serializers import (
     ActorSerializer,
+    GenreSerializer,
 )
 
 
 class ActorViewSet(viewsets.ModelViewSet):
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
