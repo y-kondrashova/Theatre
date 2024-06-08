36d981cf62b7b422d179e6bbdc64d0f295f47f4b
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-06-08 11:11:36 +0300

    Add GenreSerializer to serializers.py theatre app

--- theatre/serializers.py
+++ theatre/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
 from theatre.models import (
     Actor,
+    Genre,
 )
 
 
 class ActorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Actor
+        fields = "__all__"
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
         fields = "__all__"
