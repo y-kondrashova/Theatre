ea27b2693b0b2efb55178f60dfd73fed0f1bb42d
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-06-08 13:32:31 +0300

    Add path to theatre_halls in urls.py theatre app

--- theatre/urls.py
+++ theatre/urls.py
@@ -5,12 +5,14 @@ from theatre.views import (
     ActorViewSet,
     GenreViewSet,
     PlayViewSet,
+    TheatreHallViewSet,
 )
 
 router = routers.DefaultRouter()
 router.register("actors", ActorViewSet)
 router.register("genres", GenreViewSet)
 router.register("plays", PlayViewSet)
+router.register("theatre_halls", TheatreHallViewSet)
 
 urlpatterns = [path("", include(router.urls))]
 
