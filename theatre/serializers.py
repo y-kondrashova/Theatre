1655b748dad5d3cae28a4788a19a7cd5b808ccb9
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-06-08 15:22:18 +0300

    Update TicketSerializer
    Add 'create' and 'update' methods

--- theatre/serializers.py
+++ theatre/serializers.py
@@ -103,6 +103,30 @@ class ReservationSerializer(serializers.ModelSerializer):
 
 
 class TicketSerializer(serializers.ModelSerializer):
+    reservation = ReservationSerializer()
+
     class Meta:
         model = Ticket
         fields = ["id", "row", "seat", "performance", "reservation"]
+
+    def create(self, validated_data):
+        reservation_data = validated_data.pop("reservation")
+        reservation = Reservation.objects.create(**reservation_data)
+        ticket = Ticket.objects.create(
+            reservation=reservation, **validated_data
+        )
+        return ticket
+
+    def update(self, instance, validated_data):
+        reservation_data = validated_data.pop("reservation")
+        reservation = Reservation.objects.create(**reservation_data)
+
+        instance.row = validated_data.get("row", instance.row)
+        instance.seat = validated_data.get("seat", instance.seat)
+        instance.performance = validated_data.get(
+            "performance", instance.performance
+        )
+        instance.reservation = reservation
+        instance.save()
+
+        return instance
