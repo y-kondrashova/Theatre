ab0845034bb1f34ae46db28a09eb94b3b732c537
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-06-09 09:48:01 +0300

    Add path to register in user app

--- user/urls.py
+++ user/urls.py
@@ -4,7 +4,11 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from user.views import CreateUserView
+
+
 urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="register"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
