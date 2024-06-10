612ed89476f95505f706e48b4db26cd905fa79a4
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-06-10 16:05:08 +0300

    Add test_user_create_genre for UserCannotCreateTest

--- theatre/test/test_admin_available.py
+++ theatre/test/test_admin_available.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.reverse import reverse
 from rest_framework.test import APIClient
 
 from theatre.models import Actor, Genre, TheatreHall, Play
@@ -19,3 +21,12 @@ class UserCannotCreateTest(TestCase):
 
         self.client = APIClient()
         self.client.login(username="user", password="secretpass")
+
+    def test_user_create_genre(self):
+        self.client.credentials()
+        data = {"name": "Test Genre"}
+
+        response = self.client.post(
+            reverse("theatre:genres-list"), data, format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
