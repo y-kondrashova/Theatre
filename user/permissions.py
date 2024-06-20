b7597e7e90e9830d9371ad558ba38f4dac78291f
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-06-20 13:06:18 +0300

    Refactor IsAdminOrReadOnly
    Simplify 'has_permission' method

--- user/permissions.py
+++ user/permissions.py
@@ -7,8 +7,4 @@ class IsAdminOrReadOnly(BasePermission):
     """
 
     def has_permission(self, request, view):
-        return bool(
-            request.method in SAFE_METHODS
-            or request.user
-            and request.user.is_staff
-        )
+        return request.method in SAFE_METHODS or request.user.is_staff
